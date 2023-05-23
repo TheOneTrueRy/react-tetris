8aed8c3fc7a36ab4f879bd474d65dee0d19e6d73
Ry <OfficiallyRy@outlook.com>
2023-05-23 16:27:30 -0600

    Grid is now the proper grid instead of 'L' yellow

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,7 +3,7 @@ import { StyledCell } from "./styles/StyledCell.js";
 import { TETROMINOS } from "../tetrominos.js";
 
 const Cell = ({type}) => (
-  <StyledCell type={'L'} color={TETROMINOS['L'].color}/>
+  <StyledCell type={type} color={TETROMINOS[type].color}/>
 )
 
 export default Cell;
