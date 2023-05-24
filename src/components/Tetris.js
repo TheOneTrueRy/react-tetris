b9fef8589acbd568845724c7994249d4205c76c4
Ry <OfficiallyRy@outlook.com>
2023-05-23 18:33:24 -0600

    Pieces now automatically fall every second, except for when player is hitting down

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -13,6 +13,7 @@ import StartButton from "./StartButton.js";
 // Custom Hooks
 import {usePlayer} from '../hooks/usePlayer.js';
 import {useStage} from '../hooks/useStage.js'
+import {useInterval} from '../hooks/useInterval.js'
 
 const Tetris = () => {
   const [dropTime, setDropTime] = useState(null);
@@ -29,6 +30,7 @@ const Tetris = () => {
 
   const startGame = () => {
     setStage(createStage());
+    setDropTime(1000);
     resetPlayer();
     setGameOver(false);
   }
@@ -46,7 +48,16 @@ const Tetris = () => {
     }
   }
 
+  const keyUp({keyCode}) => {
+    if(!gameOver){
+      if(keyCode === 40){
+        setDropTime(1000);
+      }
+    }
+  }
+
   const dropPlayer = () => {
+    setDropTime(null);
     drop();
   }
 
@@ -64,8 +75,12 @@ const Tetris = () => {
     }
   }
 
+  useInterval(() => {
+    drop();
+  }, dropTime)
+
   return(
-    <StyledTetrisWrapper role="button" tabIndex={0} onKeyDown={e => move(e)}>
+    <StyledTetrisWrapper role="button" tabIndex={0} onKeyDown={e => move(e)} onKeyUp={keyUp}>
       <StyledTetris>
         <Stage stage={stage}/>
         <aside>
